86efca7fa4aaeac703118cb854a7753cc2f28ee6
Ciclonix <72094161+Ciclonix@users.noreply.github.com>
2024-09-02 14:52:00 +0200

    add methods initialize, play, checkWin and checkLetter

--- lib/hangman.rb
+++ lib/hangman.rb
@@ -1,4 +1,24 @@
 class Hangman
+  attr_accessor :word, :tries, :lives
+
+  def initialize
+    @word = getRandomWord
+    @tries = {good: [], bad: []}
+    @lives = 10
+    play ? puts("You win!") : puts("You lose!")
+  end
+
+  def play
+    printWord
+    loop do
+      new_letter = addLetter
+      checkLetter
+      return false if lives == 0
+      printWord
+      return true if checkWin
+    end
+  end
+
   def getRandomWord
     dictionary = File.read("dictionary.txt").split("\n")
     loop do
@@ -7,7 +27,8 @@ class Hangman
     end
   end
 
-  def printWord(word, tries)
+  def printWord
+    print "Word: "
     word.each do |x|
       tries.include?(x) ? print("#{x} ") : print("_ ")
     end
@@ -25,4 +46,26 @@ class Hangman
     end
     return letter
   end
+
+  def checkWin
+    return word.all? { |x| tries[:good].include? x }
+  end
+
+  def checkLetter
+    if word.include?(new_letter)
+      tries[:good] << new_letter
+      puts "The letter #{new_letter} is in the word"
+    else
+      tries[:bad] << new_letter
+      lives -= 1
+      return if lives == 0
+
+      puts "The letter #{new_letter} is not in the word"
+      puts "You have #{lives} lives left"
+    end
+    puts "Incorrect guesses: #{tries[:bad].inspect}"
+  end
 end
+
+
+Hangman.new
