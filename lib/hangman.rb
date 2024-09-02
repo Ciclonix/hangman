7d92e0ef8aa2c13b2a9834d42ecc0adf76f28896
Ciclonix <72094161+Ciclonix@users.noreply.github.com>
2024-09-02 13:42:14 +0200

    add methods printWord and addLetter

--- lib/hangman.rb
+++ lib/hangman.rb
@@ -1,7 +1,28 @@
-def getRandomWord
-  dictionary = File.read("dictionary.txt").split("\n")
-  loop do
-    word = dictionary.sample
-    return word.split("") if word.length.between?(5,12) 
+class Hangman
+  def getRandomWord
+    dictionary = File.read("dictionary.txt").split("\n")
+    loop do
+      word = dictionary.sample
+      return word.split("") if word.length.between?(5,12) 
+    end
+  end
+
+  def printWord(word, tries)
+    word.each do |x|
+      tries.include?(x) ? print("#{x} ") : print("_ ")
+    end
+    puts
+  end
+
+  def addLetter
+    begin
+      print "Try a new letter: "
+      letter = gets.chomp.downcase
+      raise "Error" unless letter.match /^[a-z]{1}$/ 
+    rescue
+      puts "Error: Invalid character"
+      retry
+    end
+    return letter
   end
 end
